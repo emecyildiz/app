01c4eee60adb2b8f570005c7b1bac8fc4431903a
Emeç Yıldız <emecyildiz@posta.mu.edu.tr>
2026-02-22 14:06:04 +0300

    vercel deployment sorunu giderme

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -58,14 +58,6 @@ const AuthCallback = () => {
   )
 }
 
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <LoadingSpinner />
-        <p className="text-gray-400 mt-4">Giriş yapılıyor...</p>
-      </div>
-    </div>
-  )
-}
+export default AuthCallback
 
 export default AuthCallback
